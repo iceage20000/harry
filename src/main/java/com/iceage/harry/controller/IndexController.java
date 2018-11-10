aaae6ef1e6abdf21eb73ee6d0e9712496dc720ad
午觉觉 <1415515184@qq.com>
2018-11-10 16:57:35 +0800

    add hook

--- src/main/java/com/iceage/harry/controller/IndexController.java
+++ src/main/java/com/iceage/harry/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.iceage.harry.controller;
 
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(description = "hello")
 @RestController
+@Slf4j
 public class IndexController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World！";
     }
+
+    @PostMapping("/github-webhook")
+    public String hook() {
+        log.info("webhook");
+        return "webhook";
+    }
+
 }
