4768712971d1f6907899dcdf15afa48ca8136912
午觉觉 <1415515184@qq.com>
2018-11-10 09:32:11 +0800

    first commit

---
+++ src/main/java/com/iceage/harry/HarryApplication.java
@@ -0,0 +1,12 @@
+package com.iceage.harry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/iceage/harry/HarryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iceage.harry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HarryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
